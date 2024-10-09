d3065e902ce30e250faec206d56e9b3c4f361cee
vedianbnka <v.bunka238@gmail.com>
2024-10-09 23:00:35 +0700

    session

--- check_session_admin.php
+++ check_session_admin.php
@@ -2,7 +2,7 @@
 session_start();
 
 // Cek apakah username ada dalam sesi
-if (isset($_SESSION['nama'])) {
+if (isset($_SESSION['role']) && $_SESSION['role'] == 'admin') {
     echo json_encode(['status' => 'active']);
 } else {
     echo json_encode(['status' => 'inactive']);
--- dashboard/index.php
+++ dashboard/index.php
@@ -3,16 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Page</title>
+    <title>Document</title>
     <script>
         function checkSession() {
   // Kirim permintaan AJAX ke check_session.php
   var xhr = new XMLHttpRequest();
-  xhr.open("GET", "../check_session_admin.php", true);
+  xhr.open("GET", "../check_session.php", true);
   xhr.onload = function () {
     if (xhr.status === 200) {
       var response = JSON.parse(xhr.responseText);
       if (response.status === "inactive") {
+        // Jika sesi tidak aktif, redirect ke halaman login
         window.location.href = "../login/index.php";
       }
     }
@@ -24,12 +25,6 @@ setInterval(checkSession, 1);
     </script>
 </head>
 <body>
-    <a href="../admin/add_event.php">Add Event</a>
-    <a href="../admin/add_admin.php">Tambah Admin Baru</a>
     <a href="../logout.php">Log Out</a>
-
-    <?php
-        require_once "../db.php";
-    ?>
 </body>
 </html>
