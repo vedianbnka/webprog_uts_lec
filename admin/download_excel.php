50005750e0d39bb496d3585a70650f176f644097
vedianbnka <v.bunka238@gmail.com>
2024-10-20 14:18:24 +0700

    excel

--- admin/download_excel.php
+++ admin/download_excel.php
@@ -6,18 +6,56 @@ use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 use PhpOffice\PhpSpreadsheet\IOFactory;
 
+$id_event = $_GET['id_event'];
+$sql = "SELECT * FROM event_konser WHERE id_event = ?";
+$statement = $db->prepare($sql);
+$statement->execute([$id_event]);
+$event = $statement->fetch(PDO::FETCH_ASSOC);
+$nama_event = $event['nama_event'];
+
 $spreadsheet = new Spreadsheet();
 $sheet = $spreadsheet->getActiveSheet();
-$sheet->setCellValue('A1', 'Hello World !');
-$sheet->setCellValue('A2','Hello UMN');
-$sheet->setCellValue('A3','Hello Indonesia');
-$sheet->setCellValue('B1','John Thor');
-$sheet->setCellValue('B2', 'John Wick');
-$sheet->setCellValue('B3','John Doe');
+$sql = "SELECT * FROM list_partisipan_event AS a JOIN user AS b ON a.id_user = b.id_user WHERE id_event = ?";
+$statement = $db->prepare($sql);
+$statement->execute([$id_event]);
+    $sheet->setCellValue('A1', 'ID Partisipan');
+    $sheet->setCellValue('B1', 'ID User');
+    $sheet->setCellValue('C1', 'Nama');
+    $sheet->setCellValue('D1', 'Email');
+    $sheet->setCellValue('E1', 'No. Telp');
+    $sheet->setCellValue('F1', 'Tipe Tiket');
+    $sheet->setCellValue('G1', 'Jumlah Pembelian');
+    $sheet->setCellValue('H1', 'Bukti Pembayaran');
+    $sheet->setCellValue('I1', 'Status');
+    $sheet->setCellValue('J1', 'No. Tiket');
+
+while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+    $id_partisipan = $row['id_partisipan'];
+    $id_user = $row['id_user'];
+    $nama = $row['nama'];
+    $email = $row['email'];
+    $no_telp = $row['no_telp'];
+    $tipe_tiket = $row['tipe_tiket'];
+    $jml_pilih = $row['jumlah'];
+    $bukti_tf = $row['bukti_pembayaran'];
+    $status = $row['status'];
+    $no_tiket = $row['no_tiket'];
+    $sheet->setCellValue('A' . ($i + 2), $id_partisipan);
+    $sheet->setCellValue('B' . ($i + 2), $id_user);
+    $sheet->setCellValue('C' . ($i + 2), $nama);
+    $sheet->setCellValue('D' . ($i + 2), $email);
+    $sheet->setCellValue('E' . ($i + 2), $no_telp);
+    $sheet->setCellValue('F' . ($i + 2), $tipe_tiket);
+    $sheet->setCellValue('G' . ($i + 2), $jml_pilih);
+    $sheet->setCellValue('H' . ($i + 2), $bukti_tf);
+    $sheet->setCellValue('I' . ($i + 2), $status);
+    $sheet->setCellValue('J' . ($i + 2), $no_tiket);
+    $i++;
+}
 
 // Prepare to download the file instead of saving it
 header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-header('Content-Disposition: attachment;filename="hello.xlsx"');
+header('Content-Disposition: attachment;filename="'.$nama_event.'.xlsx"');
 header('Cache-Control: max-age=0');
 
 // Save the spreadsheet to the output stream for download
