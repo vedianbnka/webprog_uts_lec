464220427a6a460d3af2d6e97b0ef13b058ea3f2
vedianbnka <v.bunka238@gmail.com>
2024-10-24 22:51:15 +0700

    cancel

--- dashboard/cancel_ticket.php
+++ dashboard/cancel_ticket.php
@@ -7,6 +7,10 @@ $sql = "UPDATE list_partisipan_event SET status = 'canceled', no_tiket = null WH
 $statement = $db->prepare($sql);
 $statement->execute([$id_partisipan]);
 
+$sql = "UPDATE tiket SET jumlah_sold = jumlah_sold - (SELECT jumlah FROM list_partisipan_event WHERE id_partisipan = ?) WHERE id_event = (SELECT id_event FROM list_partisipan_event WHERE id_partisipan = ?) AND tipe_tiket = (SELECT tipe_tiket FROM list_partisipan_event WHERE id_partisipan = ?)";
+$statement = $db->prepare($sql);
+$statement->execute([$id_partisipan, $id_partisipan, $id_partisipan]);
+
 $_SESSION['success'] = 'Tiket anda telah di cancel';
 header('Location: tiket.php');
 ?>
